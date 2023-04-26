fd04fed641f90953b3de26758ba78260512d0c0c
Tejas Sridhar <tejas3098@gmail.com>
2023-04-26 02:24:01 -0400

    Revert "added login button in profile dropdown"
    This reverts commit 3d82a2536d820e58ef05f1337487e64f46e8782a.

--- src/dropdown-bar/profile.js
+++ src/dropdown-bar/profile.js
@@ -1,40 +1,36 @@
 import React, { useState } from "react";
-  import { Link, useNavigate } from "react-router-dom";
-  import "./index.css";
+import { Link, useNavigate } from "react-router-dom";
+import "./index.css";
 
-  const ProfileDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const navigate = useNavigate();
+const ProfileDropdown = () => {
+const [isOpen, setIsOpen] = useState(false);
+const navigate = useNavigate();
 
-  const handleSignOut = () => {
-  console.log("Sign out clicked");
-  };
+const handleButtonClick = () => {
+setIsOpen(!isOpen);
+};
 
-  const handleAccount = () => {
-  navigate("/profile");
-  setIsOpen(false);
-  };
+const handleSignOut = () => {
+console.log("Sign out clicked");
+};
 
-  const handleLogin = () => {
-  navigate("/login");
-  setIsOpen(false);
-  };
-    
-  return (
-  <div className="profile-dropdown">
-  <ul>
-  <li>
-  <div onClick={handleAccount}>Account</div>
-  </li>
-  <li>
-  <div onClick={handleLogin}>Login</div>
-  </li>
-  <li>
-  <div onClick={handleSignOut}>Sign Out</div>
-  </li>
-  </ul>
-  </div>
-  );
-  };
+const handleAccount = () => {
+navigate("/profile");
+setIsOpen(false);
+};
+
+return (
+<div className="profile-dropdown">
+<ul>
+<li>
+<div onClick={handleAccount}>Account</div>
+</li>
+<li>
+<div onClick={handleSignOut}>Sign Out</div>
+</li>
+</ul>
+</div>
+);
+};
 
 export default ProfileDropdown;
