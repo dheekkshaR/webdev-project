3d82a2536d820e58ef05f1337487e64f46e8782a
Tejas Sridhar <tejas3098@gmail.com>
2023-04-26 00:43:57 -0400

    added login button in profile dropdown

--- src/dropdown-bar/profile.js
+++ src/dropdown-bar/profile.js
@@ -1,36 +1,40 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./index.css";
+  import { Link, useNavigate } from "react-router-dom";
+  import "./index.css";
 
-const ProfileDropdown = () => {
-const [isOpen, setIsOpen] = useState(false);
-const navigate = useNavigate();
+  const ProfileDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
-const handleButtonClick = () => {
-setIsOpen(!isOpen);
-};
+  const handleSignOut = () => {
+  console.log("Sign out clicked");
+  };
 
-const handleSignOut = () => {
-console.log("Sign out clicked");
-};
+  const handleAccount = () => {
+  navigate("/profile");
+  setIsOpen(false);
+  };
 
-const handleAccount = () => {
-navigate("/profile");
-setIsOpen(false);
-};
-
-return (
-<div className="profile-dropdown">
-<ul>
-<li>
-<div onClick={handleAccount}>Account</div>
-</li>
-<li>
-<div onClick={handleSignOut}>Sign Out</div>
-</li>
-</ul>
-</div>
-);
-};
+  const handleLogin = () => {
+  navigate("/login");
+  setIsOpen(false);
+  };
+    
+  return (
+  <div className="profile-dropdown">
+  <ul>
+  <li>
+  <div onClick={handleAccount}>Account</div>
+  </li>
+  <li>
+  <div onClick={handleLogin}>Login</div>
+  </li>
+  <li>
+  <div onClick={handleSignOut}>Sign Out</div>
+  </li>
+  </ul>
+  </div>
+  );
+  };
 
 export default ProfileDropdown;
